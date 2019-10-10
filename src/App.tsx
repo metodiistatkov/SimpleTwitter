7f2083838394f8f3d6d056d8e8e94130b2a3583c
Metodi <metodi.istatkov@gmail.com>
2019-10-10 22:14:15 +0100

    created backgound

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const title: Object = 
+<div
+  style={{
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }}
+>
+  <h1>TwitterSoton</h1>
+</div>
+
+class App extends React.Component{
+  
+  render(){
+    return title;
+  }
 }
 
 export default App;
